aae87c16d4e01ab0086e051ae77c924ca243d225
NeptuneRjo <rjo9000@outlook.com>
2025-09-07 17:34:51 -0400

    Setup bones for routing
    Created indexs for routes
    
    Added navigation

---
+++ client/app/params/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Params = () => {
+  return (
+    <View>
+      <Text>Params</Text>
+    </View>
+  )
+}
+
+export default Params
+
+const styles = StyleSheet.create({})
