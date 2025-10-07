32535583d1bd84f41424506b8662ab9d4e4393fc
NeptuneRjo <rjo9000@outlook.com>
2025-10-07 07:18:40 -0400

    Added service methods for create, update, and delete

--- client/services/tankService.ts
+++ client/services/tankService.ts
@@ -1,25 +1,65 @@
 import { ApiResponse, Tank, Tanks } from "../types"
-import Config from "react-native-config"
 
 const BASE_ROUTE = process.env.EXPO_PUBLIC_API_URL
 
-const getAllTanks = async (clerk_id: string): Promise<ApiResponse<Tanks[]>> => {
+const getAllTanks = async (
+    clerk_id: string
+): Promise<ApiResponse<Tanks[]>> => {
     return fetch(`${BASE_ROUTE}/api/tanks`, { 
         headers: { 'Clerk-Id': clerk_id }, 
         method: 'GET'
     }).then(res => res.json())
 }
 
-const getTank = async (clerk_id: string, ulid: string): Promise<ApiResponse<Tank>> => {
+const getTank = async (
+    clerk_id: string, 
+    ulid: string
+): Promise<ApiResponse<Tank>> => {
     return fetch(`${BASE_ROUTE}/api/tanks/${ulid}`, {
         headers: { 'Clerk-Id': clerk_id },
         method: 'GET'
     }).then(res => res.json())
 }
 
+const createTank = async (
+    clerk_id: string, 
+    item: { name: string, description: string | undefined }
+): Promise<ApiResponse<Tank>> => {
+    return fetch(`${BASE_ROUTE}/api/tanks`, {
+        headers: { 'Clerk-Id': clerk_id },
+        method: 'POST',
+        body: JSON.stringify(item)
+    }).then(res => res.json())
+}
+
+const updateTank = async (
+    clerk_id: string, 
+    tank_ulid: string, 
+    item: { name: string | undefined, description: string | undefined}
+): Promise<ApiResponse<Tank>> => {
+    return fetch(`${BASE_ROUTE}/api/tanks/${tank_ulid}`, {
+        headers: { 'Clerk-Id': clerk_id },
+        method: 'PATCH',
+        body: JSON.stringify(item)
+    }).then(res => res.json())
+}
+
+const deleteTank = async (
+    clerk_id: string, 
+    tank_ulid: string
+): Promise<ApiResponse<[]>> => {
+    return fetch(`${BASE_ROUTE}/api/tanks/${tank_ulid}`, {
+        headers: { 'Clerk-Id': clerk_id },
+        method: 'DELETE'
+    }).then(res => res.json())
+}
+
 const TankService = {
     getAllTanks,
-    getTank
+    getTank,
+    updateTank,
+    createTank,
+    deleteTank
 }
 
 export default TankService
