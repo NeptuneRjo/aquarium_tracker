f95ae222ecad30f6abd1f090b7e594fb1d2a6b48
NeptuneRjo <rjo9000@outlook.com>
2025-11-01 14:32:14 -0400

    Added modal for updating and deleting tank

--- client/app/tank/[id].tsx
+++ client/app/tank/[id].tsx
@@ -1,14 +1,15 @@
-import { StyleSheet, Text, useWindowDimensions, View } from 'react-native'
+import { Modal, Pressable, StyleSheet, Text, TextInput, useWindowDimensions, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import GlobalStyles from '../../constants/styles'
 import TankService from '../../services/tankService'
 import { useUser } from '@clerk/clerk-expo'
-import { Stack, useLocalSearchParams } from 'expo-router'
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router'
 import { Tank as TankType } from '../../types'
 import { SceneMap, TabBar, TabView } from 'react-native-tab-view'
 import ParamView from '../../components/ParamView'
 import Colors from '../../constants/colors'
 import TankStorage from '../../services/tankStorage'
+import Button from '../../components/Button'
 
 interface Route {
   key: string,
@@ -19,6 +20,8 @@ const Tank = () => {
   const layout = useWindowDimensions()
   const { user, isSignedIn, isLoaded: clerkIsLoaded } = useUser()
   const { id } = useLocalSearchParams()
+  const navigation = useRouter()
+  
   
   const [tank, setTank] = useState<TankType>()
   const [error, setError] = useState<any>()
@@ -26,12 +29,17 @@ const Tank = () => {
   
   const [index, setIndex] = React.useState(0)
   
+  const [modalVisible, setModalVisible] = useState<boolean>(false)
+  const [tankName, setTankName] = useState<string | undefined>()
+  const [tankDescription, setTankDescription] = useState<string | undefined>()
+  const [modalLoading, setModalLoading] = useState<boolean>(false)
+
   const getAndSetTank = async () => {
     if (isSignedIn) {
       const jsonValue = await TankStorage.getTank(id as string)
 
       if (jsonValue !== null) {
-        setTank(JSON.parse(jsonValue))
+        setTank(jsonValue)
       } else {
         TankService.getTank(user.id, id as string)
         .then(async ({ data }) => {
@@ -41,6 +49,36 @@ const Tank = () => {
         .catch((err) => setError(err)) 
       }
       setIsLoading(false)
+    }
+  }
+
+  const deleteTank = async () => {
+    if (isSignedIn && tank !== undefined) {
+      setModalLoading(true)
+      TankService.deleteTank(user.id, tank.ulid)
+        .then(async () => {
+          TankStorage.removeTank(tank.ulid)
+            .then(async () => {
+              await TankStorage.removeAllTanks()
+              setModalVisible(false)
+              navigation.dismissAll()
+              navigation.replace('/')
+            })
+        }).catch((err) => console.log(err))
+    }
+  }
+
+  const updateTank = async () => {
+    if (isSignedIn && tank !== undefined) { 
+      setModalLoading(true)
+      const item = { name: tankName, description: tankDescription }
+      console.log(item)
+      TankService.updateTank(user.id, tank.ulid, item)
+        .then(async ({ data }) => {
+          await TankStorage.removeAllTanks()
+          await TankStorage.storeTank(data) 
+          setModalVisible(false)
+        })
     }
   }
   
@@ -70,7 +108,62 @@ const Tank = () => {
 
   return (
     <>
-      <Stack.Screen options={{ headerTitle: tank.name }} />
+      <Stack.Screen 
+        options={{ 
+          headerTitle: tank.name,
+          headerRight: (props) => (
+            <Pressable {...props} onPress={() => setModalVisible(true)}>
+              <Text>Edit</Text>
+            </Pressable>
+          ),
+        }} 
+      />
+      <Modal 
+        backdropColor={Colors.secondary} 
+        visible={modalVisible} 
+        animationType='fade'
+      >
+        <View style={{ padding: 12, gap: 16, display: 'flex', flex: 1 }}>
+          <View style={styles.modalCtrls}>
+            <Text style={{ fontWeight: 'bold', fontSize: 18 }}>Editing {tank.name}</Text>
+            <Pressable onPress={() => setModalVisible(false)}>
+              <Text style={{ fontWeight: 'semibold', fontSize: 16 }}>Back</Text>
+            </Pressable>
+          </View>
+          <View style={{ padding: 0, margin: 0, flex: 3, gap: 22 }}>
+            <View style={{ gap: 8 }}>
+              <Text style={styles.label}>New Name:</Text>
+              <TextInput 
+                value={tankName}
+                onChangeText={setTankName}
+                style={styles.name}
+                placeholder={tank.name}
+                maxLength={64}
+              />
+            </View>
+            <View style={{ gap: 8 }}>
+              <Text style={styles.label}>New Description:</Text>
+              <TextInput 
+                value={tankDescription}
+                onChangeText={setTankDescription}
+                style={styles.description}
+                maxLength={256}
+                editable
+                multiline
+                placeholder={tank.description}
+              />
+            </View>
+          </View>
+          <View style={[{ flexDirection: 'row', justifyContent: 'space-between', flex: 1 }]}>
+            <Button variant='warn' onPress={() => deleteTank()}>
+              Delete Tank
+            </Button>
+            <Button variant="primary" onPress={() => updateTank()} disabled={ !tankDescription && !tankName ? true : false }>
+              Confirm Changes
+            </Button>
+          </View>
+        </View>
+      </Modal>
       <TabView 
         navigationState={{ index, routes }}
         renderScene={renderScene}
@@ -108,5 +201,31 @@ const styles = StyleSheet.create({
   tabView: {
     margin: 16,
     marginRight: 27,
+  },
+  label: {
+    // marginHorizontal: 12,
+    color: Colors.primary,
+    fontSize: 16,
+    // marginTop: 12,
+    fontWeight: 500
+  },
+  name: {
+    height: 40,
+    // margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: Colors.border
+  },
+  description: {
+    height: 120,
+    // margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: Colors.border
+  },
+  modalCtrls: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 18,
   }
 })
