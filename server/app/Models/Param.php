f917721197097c107aebcd96663da859b65969b8
NeptuneRjo <rjo9000@outlook.com>
2025-11-14 21:46:28 -0500

    Updated belongsto method to singular spelling

--- server/app/Models/Param.php
+++ server/app/Models/Param.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
 
 class Param extends Model
@@ -25,7 +26,7 @@ class Param extends Model
         return $this->hasMany(ParamNodes::class);
     }
 
-    public function tanks()
+    public function tank()
     {
         return $this->belongsTo(Tank::class);
     }
